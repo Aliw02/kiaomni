448e77f9a06f681fe28da1da287db93998912c9b
aliweyabood <151720952+Aliw02@users.noreply.github.com>
2026-09-21 17:17:34 +0300

    feat(api): export MoE route stability controls

--- kiaomni/__init__.py
+++ kiaomni/__init__.py
@@ -10,12 +10,20 @@ from .adapters import (
 )
 from .loading import ATTN_FALLBACK_CHAIN, load_model
 from .monkey_patch import apply_kiaomni, remove_kiaomni
+from .moe_stability import (
+    AdaptiveMoERouteController,
+    apply_moe_route_stability,
+    remove_moe_route_stability,
+)
 from .policies import POLICY_REGISTRY, get_policy, register_policy
 from ._version import __version__
 
 __all__ = [
     "apply_kiaomni",
     "remove_kiaomni",
+    "apply_moe_route_stability",
+    "remove_moe_route_stability",
+    "AdaptiveMoERouteController",
     "load_model",
     "ATTN_FALLBACK_CHAIN",
     "POLICY_REGISTRY",
